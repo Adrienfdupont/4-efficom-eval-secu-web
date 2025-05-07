aee3fe48fb9e7eb6d94b99c495ea885ea6dd713d
Adrienfdupont <adrienf.dupont@gmail.com>
2025-05-07 12:28:20 +0200

    block IPs

--- middleware/limiter.middleware.js
+++ middleware/limiter.middleware.js
@@ -1,8 +1,11 @@
+import forbidden from '../forbidden.json';
+
 const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000,
 	limit: 100,
 	standardHeaders: 'draft-8',
 	legacyHeaders: false,
+	skip: (req, res) => !forbidden.addresses.includes(req.ip),
 })
 
 module.exports = limiter;
